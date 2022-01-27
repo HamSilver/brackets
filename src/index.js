2fae0eed7771873cc6b8cc590cc0c96cfd665bfe
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-01-27 13:05:43 +0200

    feat: Solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, c) {
+  let m = new Map(c);
+  let st = [];
+  s.split``.forEach(e => e === m.get(st[st.length - 1]) ? st.pop() : st.push(e));
+  return st.length == 0;
 }
